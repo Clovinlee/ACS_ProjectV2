6b52466c5c205ab84d27efcda69eb39742d930bf
HibikiXz <kevinarianto50@gmail.com>
2021-12-24 09:47:16 +0700

    Master Account

---
+++ Project_ACS/Master/Add_Akun.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ACS.Master
+{
+    public partial class Add_Akun : Form
+    {
+        public Add_Akun(Master_Akun form1)
+        {
+            parent = form1;
+            InitializeComponent();
+        }
+        DataSet ds;
+        DataSet dataset;
+        Master_Akun parent = null;
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "" && txtPass.Text != "" && txtNama.Text != "" && txtAlamat.Text != "" && txtEmail.Text != "" && cbb_jabatan.SelectedIndex != -1)
+            {
+                ds = new DataSet();
+                DB.executeDataSet(ds, $"SELECT count(*) FROM USERS WHERE username = '{txtUsername.Text}'", null , "listuser");
+                DataTable dtuser = ds.Tables["listuser"];
+                if (dtuser.Rows[0].ItemArray[0].ToString() == "0")
+                {
+                    dataset = new DataSet();
+                    String querystr = "SELECT u.USERNAME AS \"Username\", u.NAMA AS \"Nama\", j.NAMA AS \"Jabatan\", u.ALAMAT AS \"Alamat\", u.EMAIL AS \"Email\" FROM USERS u , JABATAN j WHERE u.ID_JABATAN = j.ID";
+                    DB.executeDataSet(dataset, querystr, null, "USERS");
+                    DataTable dtlistuser = dataset.Tables["USERS"];
+                    int rowid = dtlistuser.Rows.Count;
+                    DB.executeQuery($"INSERT INTO USERS VALUES({rowid+1},'{txtNama.Text}','{txtUsername.Text}','{txtPass.Text}',{cbb_jabatan.SelectedIndex+1},'{txtAlamat.Text}','{txtEmail.Text}',1)", null);
+                    MessageBox.Show("Insert success!");
+                    parent.loadAkun();
+                    this.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("User Sudah Dipakai");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Masih ada Field yang Kosong!");
+            }
+        }
+
+        private void cbb_jabatan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAlamat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNama_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project_ACS/Master/Detail_Akun.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace Project_ACS.Master
+{
+    partial class Detail_Akun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Detail_Akun));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
+            this.btn_ok = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbb_jabatan = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.AllowAnimations = true;
+            this.btn_ok.AllowMouseEffects = true;
+            this.btn_ok.AllowToggling = false;
+            this.btn_ok.AnimationSpeed = 200;
+            this.btn_ok.AutoGenerateColors = false;
+            this.btn_ok.AutoRoundBorders = false;
+            this.btn_ok.AutoSizeLeftIcon = true;
+            this.btn_ok.AutoSizeRightIcon = true;
+            this.btn_ok.BackColor = System.Drawing.Color.Transparent;
+            this.btn_ok.BackColor1 = System.Drawing.Color.MediumSpringGreen;
+            this.btn_ok.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_ok.BackgroundImage")));
+            this.btn_ok.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btn_ok.ButtonText = "Update";
+            this.btn_ok.ButtonTextMarginLeft = 0;
+            this.btn_ok.ColorContrastOnClick = 45;
+            this.btn_ok.ColorContrastOnHover = 45;
+            this.btn_ok.Cursor = System.Windows.Forms.Cursors.Default;
+            borderEdges1.BottomLeft = true;
+            borderEdges1.BottomRight = true;
+            borderEdges1.TopLeft = true;
+            borderEdges1.TopRight = true;
+            this.btn_ok.CustomizableEdges = borderEdges1;
+            this.btn_ok.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btn_ok.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btn_ok.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btn_ok.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btn_ok.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+            this.btn_ok.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_ok.ForeColor = System.Drawing.Color.Black;
+            this.btn_ok.IconLeftAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_ok.IconLeftCursor = System.Windows.Forms.Cursors.Default;
+            this.btn_ok.IconLeftPadding = new System.Windows.Forms.Padding(11, 3, 3, 3);
+            this.btn_ok.IconMarginLeft = 11;
+            this.btn_ok.IconPadding = 10;
+            this.btn_ok.IconRightAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_ok.IconRightCursor = System.Windows.Forms.Cursors.Default;
+            this.btn_ok.IconRightPadding = new System.Windows.Forms.Padding(3, 3, 7, 3);
+            this.btn_ok.IconSize = 25;
+            this.btn_ok.IdleBorderColor = System.Drawing.Color.DodgerBlue;
+            this.btn_ok.IdleBorderRadius = 1;
+            this.btn_ok.IdleBorderThickness = 1;
+            this.btn_ok.IdleFillColor = System.Drawing.Color.MediumSpringGreen;
+            this.btn_ok.IdleIconLeftImage = null;
+            this.btn_ok.IdleIconRightImage = null;
+            this.btn_ok.IndicateFocus = false;
+            this.btn_ok.Location = new System.Drawing.Point(86, 338);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btn_ok.OnDisabledState.BorderRadius = 1;
+            this.btn_ok.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btn_ok.OnDisabledState.BorderThickness = 1;
+            this.btn_ok.OnDisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btn_ok.OnDisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btn_ok.OnDisabledState.IconLeftImage = null;
+            this.btn_ok.OnDisabledState.IconRightImage = null;
+            this.btn_ok.onHoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            this.btn_ok.onHoverState.BorderRadius = 1;
+            this.btn_ok.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btn_ok.onHoverState.BorderThickness = 1;
+            this.btn_ok.onHoverState.FillColor = System.Drawing.Color.Aquamarine;
+            this.btn_ok.onHoverState.ForeColor = System.Drawing.Color.Black;
+            this.btn_ok.onHoverState.IconLeftImage = null;
+            this.btn_ok.onHoverState.IconRightImage = null;
+            this.btn_ok.OnIdleState.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.btn_ok.OnIdleState.BorderRadius = 1;
+            this.btn_ok.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btn_ok.OnIdleState.BorderThickness = 1;
+            this.btn_ok.OnIdleState.FillColor = System.Drawing.Color.MediumSpringGreen;
+            this.btn_ok.OnIdleState.ForeColor = System.Drawing.Color.Black;
+            this.btn_ok.OnIdleState.IconLeftImage = null;
+            this.btn_ok.OnIdleState.IconRightImage = null;
+            this.btn_ok.OnPressedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btn_ok.OnPressedState.BorderRadius = 1;
+            this.btn_ok.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btn_ok.OnPressedState.BorderThickness = 1;
+            this.btn_ok.OnPressedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            this.btn_ok.OnPressedState.ForeColor = System.Drawing.Color.White;
+            this.btn_ok.OnPressedState.IconLeftImage = null;
+            this.btn_ok.OnPressedState.IconRightImage = null;
+            this.btn_ok.Size = new System.Drawing.Size(112, 32);
+            this.btn_ok.TabIndex = 68;
+            this.btn_ok.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_ok.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btn_ok.TextMarginLeft = 0;
+            this.btn_ok.TextPadding = new System.Windows.Forms.Padding(0);
+            this.btn_ok.UseDefaultRadiusAndThickness = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(13, 285);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 15);
+            this.label7.TabIndex = 67;
+            this.label7.Text = "Jabatan:";
+            // 
+            // cbb_jabatan
+            // 
+            this.cbb_jabatan.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cbb_jabatan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_jabatan.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb_jabatan.FormattingEnabled = true;
+            this.cbb_jabatan.Items.AddRange(new object[] {
+            "Master Account",
+            "Warehouse Manager",
+            "Pegawai Warehouse"});
+            this.cbb_jabatan.Location = new System.Drawing.Point(86, 282);
+            this.cbb_jabatan.Margin = new System.Windows.Forms.Padding(2);
+            this.cbb_jabatan.Name = "cbb_jabatan";
+            this.cbb_jabatan.Size = new System.Drawing.Size(178, 23);
+            this.cbb_jabatan.TabIndex = 66;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(13, 243);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 65;
+            this.label6.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(86, 240);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(178, 23);
+            this.txtEmail.TabIndex = 64;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(13, 202);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 63;
+            this.label5.Text = "Alamat:";
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAlamat.Location = new System.Drawing.Point(86, 199);
+            this.txtAlamat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(178, 23);
+            this.txtAlamat.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(13, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 15);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "Nama:";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNama.Location = new System.Drawing.Point(86, 158);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(178, 23);
+            this.txtNama.TabIndex = 60;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(13, 121);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "Password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(86, 118);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(178, 23);
+            this.txtPass.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(86, 77);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(178, 23);
+            this.txtUsername.TabIndex = 56;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(220)))), ((int)(((byte)(228)))));
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 25);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Detail Account";
+            // 
+            // Detail_Akun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(73)))), ((int)(((byte)(139)))));
+            this.ClientSize = new System.Drawing.Size(277, 392);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbb_jabatan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Detail_Akun";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Detail_Akun";
+            this.Load += new System.EventHandler(this.Detail_Akun_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuButton.BunifuButton btn_ok;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbb_jabatan;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+    }
+}
