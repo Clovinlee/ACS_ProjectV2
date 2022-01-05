0e494100b81b7ba2767e23c08c4e5dccebc2961b
Enrico Adi <enrico_a20@mhs.istts.ac.id>
2022-01-06 00:24:33 +0700

    Inventory tracking selesai :v

--- Project_ACS/Manager/Detail_Tracking.Designer.cs
+++ Project_ACS/Manager/Detail_Tracking.Designer.cs
@@ -29,8 +29,21 @@ namespace Project_ACS.Manager
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.labelNamaBarang = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dgv_barang1 = new Bunifu.UI.WinForms.BunifuDataGridView();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_barang1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelNamaBarang
@@ -56,17 +69,146 @@ namespace Project_ACS.Manager
             this.label1.TabIndex = 19;
             this.label1.Text = "Tracking Barang";
             // 
+            // dgv_barang1
+            // 
+            this.dgv_barang1.AllowCustomTheming = false;
+            this.dgv_barang1.AllowUserToAddRows = false;
+            this.dgv_barang1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            this.dgv_barang1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_barang1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_barang1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_barang1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv_barang1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgv_barang1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(115)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_barang1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_barang1.ColumnHeadersHeight = 40;
+            this.dgv_barang1.CurrentTheme.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
+            this.dgv_barang1.CurrentTheme.AlternatingRowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgv_barang1.CurrentTheme.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.dgv_barang1.CurrentTheme.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.dgv_barang1.CurrentTheme.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgv_barang1.CurrentTheme.BackColor = System.Drawing.Color.White;
+            this.dgv_barang1.CurrentTheme.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(238)))), ((int)(((byte)(255)))));
+            this.dgv_barang1.CurrentTheme.HeaderStyle.BackColor = System.Drawing.Color.DodgerBlue;
+            this.dgv_barang1.CurrentTheme.HeaderStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            this.dgv_barang1.CurrentTheme.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgv_barang1.CurrentTheme.HeaderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(115)))), ((int)(((byte)(204)))));
+            this.dgv_barang1.CurrentTheme.HeaderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.dgv_barang1.CurrentTheme.Name = null;
+            this.dgv_barang1.CurrentTheme.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgv_barang1.CurrentTheme.RowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgv_barang1.CurrentTheme.RowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.dgv_barang1.CurrentTheme.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            this.dgv_barang1.CurrentTheme.RowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_barang1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv_barang1.EnableHeadersVisualStyles = false;
+            this.dgv_barang1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(238)))), ((int)(((byte)(255)))));
+            this.dgv_barang1.HeaderBackColor = System.Drawing.Color.DodgerBlue;
+            this.dgv_barang1.HeaderBgColor = System.Drawing.Color.Empty;
+            this.dgv_barang1.HeaderForeColor = System.Drawing.Color.White;
+            this.dgv_barang1.Location = new System.Drawing.Point(16, 85);
+            this.dgv_barang1.Margin = new System.Windows.Forms.Padding(2);
+            this.dgv_barang1.MultiSelect = false;
+            this.dgv_barang1.Name = "dgv_barang1";
+            this.dgv_barang1.ReadOnly = true;
+            this.dgv_barang1.RowHeadersVisible = false;
+            this.dgv_barang1.RowHeadersWidth = 51;
+            this.dgv_barang1.RowTemplate.Height = 40;
+            this.dgv_barang1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_barang1.Size = new System.Drawing.Size(814, 384);
+            this.dgv_barang1.TabIndex = 42;
+            this.dgv_barang1.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.Light;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 15;
+            this.bunifuElipse1.TargetControl = this.dgv_barang1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(16, 490);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(138, 60);
+            this.groupBox1.TabIndex = 43;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(25, 16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(13, 13);
+            this.panel3.TabIndex = 44;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(25, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(13, 13);
+            this.panel2.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Barang Keluar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Barang Masuk";
+            // 
             // Detail_Tracking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(106)))));
-            this.ClientSize = new System.Drawing.Size(373, 330);
+            this.ClientSize = new System.Drawing.Size(848, 571);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgv_barang1);
             this.Controls.Add(this.labelNamaBarang);
             this.Controls.Add(this.label1);
             this.Name = "Detail_Tracking";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Detail_Tracking";
             this.Load += new System.EventHandler(this.Detail_Tracking_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_barang1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +218,12 @@ namespace Project_ACS.Manager
 
         private System.Windows.Forms.Label labelNamaBarang;
         private System.Windows.Forms.Label label1;
+        private Bunifu.UI.WinForms.BunifuDataGridView dgv_barang1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Project_ACS/Manager/Detail_Tracking.cs
+++ Project_ACS/Manager/Detail_Tracking.cs
@@ -16,10 +16,86 @@ namespace Project_ACS.Manager
         {
             InitializeComponent();
         }
-
+        public Detail_Tracking(String nama, String warehouse, int idbarang, int idwarehouse)
+        {
+            InitializeComponent();
+            labelNamaBarang.Text = nama+" di "+warehouse;
+            DataSet ds_barang = new DataSet(); 
+            List<object[]> listParam = new List<object[]>();
+            listParam.Add(new object[] {idwarehouse, "int32" });
+            listParam.Add(new object[] { idbarang, "int32" });
+            DB.executeDataSet(ds_barang, "SELECT H.TANGGAL, H.QTY, h.status, h.keterangan  FROM BARANG B JOIN HISTORY_BARANG_KELUAR_MASUK H ON B.ID = H.ID_BARANG WHERE H.ID_WAREHOUSE = :0 and B.ID = :1 order by h.TANGGAL desc", listParam, "invbarang");
+            dgv_barang1.DataSource = ds_barang.Tables["invbarang"]; 
+            dgv_barang1.DefaultCellStyle.SelectionBackColor = Color.FromArgb(0, 102, 204);
+            dgv_barang1.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgv_barang1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 135, 224);
+            dgv_barang1.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(44, 135, 224); 
+            for (int i = 0; i < dgv_barang1.ColumnCount; i++)
+            {
+                dgv_barang1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            dgv_barang1.Columns["status"].Visible = false; 
+            ds_barang.Tables["invbarang"].Columns.Add("Asal");
+            ds_barang.Tables["invbarang"].Columns.Add("Tujuan");
+            ds_barang.Tables["invbarang"].Columns.Add("Keterangan Tracking");
+            namawarehouse = warehouse;
+            idw = idwarehouse;
+            idb = idbarang;
+        }
+        string namawarehouse = ""; int idw = 0; int idb = 0;
         private void Detail_Tracking_Load(object sender, EventArgs e)
         {
+            for (int i = 0; i < dgv_barang1.RowCount; i++)
+            { 
+                string[] list = dgv_barang1.Rows[i].Cells["keterangan"].Value.ToString().Split('-');
+                string detail = list[0];
+                int idx = Convert.ToInt32(list[1]);  
+                String tujuan = "";
+                List<object[]> listParam = new List<object[]>();
+                listParam.Add(new object[] { idx , "int32" });
+
+                if (dgv_barang1.Rows[i].Cells["status"].Value.ToString() == "0")
+                {
+                    dgv_barang1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(224, 128, 128); //keluar = merah  
+                    dgv_barang1.Rows[i].Cells["Asal"].Value = namawarehouse;
+                  
+                    if (detail == "P")
+                    { 
+                        dgv_barang1.Rows[i].Cells["Keterangan Tracking"].Value = "Pindah Keluar";
+                        tujuan = DB.executeScalar("SELECT NAMA FROM WAREHOUSE WHERE ID = :0", listParam).ToString();
+                    }
+                    else
+                    {  
+                        dgv_barang1.Rows[i].Cells["Keterangan Tracking"].Value = "Delivery Keluar";
+                        tujuan = DB.executeScalar("SELECT NAMA FROM BUSINESS_PARTNER WHERE ID = :0 ", listParam).ToString();
+                    }
+                    dgv_barang1.Rows[i].Cells["Tujuan"].Value = tujuan;
+                }
+                else
+                {
+                    dgv_barang1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(192, 255, 192); //masuk = hijau  
+                    dgv_barang1.Rows[i].Cells["Tujuan"].Value = namawarehouse;
+
+                    if (detail == "P")
+                    {
+                        dgv_barang1.Rows[i].Cells["Keterangan Tracking"].Value = "Pindah Masuk";
+                        tujuan = DB.executeScalar("SELECT NAMA FROM WAREHOUSE WHERE ID = :0", listParam).ToString();
+                    }
+                    else
+                    {
+                        dgv_barang1.Rows[i].Cells["Keterangan Tracking"].Value = "Delivery Masuk";
+                        tujuan = DB.executeScalar("SELECT NAMA FROM BUSINESS_PARTNER WHERE ID = :0 ", listParam).ToString();
+                    }
+                    dgv_barang1.Rows[i].Cells["Asal"].Value = tujuan;
+                }
+                 
+            }
+            dgv_barang1.Columns["keterangan"].Visible = false;
 
         }
+        private void isiKolomSisa() { 
+            
+        }
+
     }
 }
