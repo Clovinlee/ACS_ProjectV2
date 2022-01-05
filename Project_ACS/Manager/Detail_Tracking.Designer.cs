d0bcb75086c2b1f55e27391ab218e36e2867ebe6
Enrico Adi <enrico_a20@mhs.istts.ac.id>
2022-01-05 20:54:17 +0700

    database history barang keluar masuk edit

---
+++ Project_ACS/Manager/Detail_Tracking.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace Project_ACS.Manager
+{
+    partial class Detail_Tracking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNamaBarang = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelNamaBarang
+            // 
+            this.labelNamaBarang.AutoSize = true;
+            this.labelNamaBarang.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNamaBarang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(220)))), ((int)(((byte)(228)))));
+            this.labelNamaBarang.Location = new System.Drawing.Point(12, 46);
+            this.labelNamaBarang.Name = "labelNamaBarang";
+            this.labelNamaBarang.Size = new System.Drawing.Size(127, 21);
+            this.labelNamaBarang.TabIndex = 20;
+            this.labelNamaBarang.Text = "<Nama barang>";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(220)))), ((int)(((byte)(228)))));
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 32);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tracking Barang";
+            // 
+            // Detail_Tracking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(106)))));
+            this.ClientSize = new System.Drawing.Size(373, 330);
+            this.Controls.Add(this.labelNamaBarang);
+            this.Controls.Add(this.label1);
+            this.Name = "Detail_Tracking";
+            this.Text = "Detail_Tracking";
+            this.Load += new System.EventHandler(this.Detail_Tracking_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNamaBarang;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Project_ACS/Manager/Detail_Tracking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ACS.Manager
+{
+    public partial class Detail_Tracking : Form
+    {
+        public Detail_Tracking()
+        {
+            InitializeComponent();
+        }
+
+        private void Detail_Tracking_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
