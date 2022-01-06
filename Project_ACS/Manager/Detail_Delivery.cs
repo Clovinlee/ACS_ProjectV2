498a876ac1b0e61ed2e3936982a2f5343d54f1e0
HibikiXz <kevinarianto50@gmail.com>
2022-01-06 13:10:11 +0700

    Delivery History

--- Project_ACS/Manager/Detail_Delivery.cs
+++ Project_ACS/Manager/Detail_Delivery.cs
@@ -20,13 +20,25 @@ namespace Project_ACS.Manager
         }
         public String kode;
         Delivery_History parent = null;
+        String querystr;
+        DataSet dataset;
         private void bunifuButton1_Click(object sender, EventArgs e)
         {
             if(cbb_partner.SelectedIndex != -1)
             {
                 if(cbb_partner.SelectedIndex == 0)
                 {
+                    dataset = new DataSet();
                     DB.executeQuery($"UPDATE H_ORDER_SUPPLIER SET STATUS = 3 WHERE KODE = '{kode}'", null);
+                    querystr = $"SELECT B.ID , B.QTY , DO.QTY FROM BARANG B ,  D_ORDER_SUPPLIER DO WHERE DO.KODE_ORDER = '{kode}' AND DO.ID_BARANG = B.ID";
+                    DB.executeDataSet(dataset,querystr, null,"BARANG");
+                    for (int n = 0; n < dataset.Tables["BARANG"].Rows.Count;n++)
+                    {
+                        int tambahan = Convert.ToInt32(dataset.Tables["BARANG"].Rows[n].ItemArray[1].ToString()) + Convert.ToInt32(dataset.Tables["BARANG"].Rows[n].ItemArray[2].ToString());
+                        String angkatambah = tambahan.ToString();
+                        querystr = $"UPDATE BARANG_WAREHOUSE SET QTY = {angkatambah} WHERE ID_BARANG = {dataset.Tables["BARANG"].Rows[n].ItemArray[0].ToString()} AND ID_WAREHOUSE = {User.User_login.Id_warehouse}";
+                        DB.executeQuery(querystr, null);
+                    }
                 }
                 else if (cbb_partner.SelectedIndex == 1)
                 {
